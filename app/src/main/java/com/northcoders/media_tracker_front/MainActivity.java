9c3af52b2610045333cb8671963508df58ae7acd
Shamar <Shamar098@gmail.com>
2025-01-13 15:32:03 +0000

    feat: implement Watched tab fragment/ Correct logic for nav bar

--- app/src/main/java/com/northcoders/media_tracker_front/MainActivity.java
+++ app/src/main/java/com/northcoders/media_tracker_front/MainActivity.java
@@ -3,23 +3,24 @@ package com.northcoders.media_tracker_front;
 import android.os.Bundle;
 import android.view.MenuItem;
 
-import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
 
 import com.google.android.material.navigation.NavigationBarView;
 import com.northcoders.media_tracker_front.fragments.BookmarkedFragment;
 import com.northcoders.media_tracker_front.fragments.CurrentlyWatchingFragment;
 import com.northcoders.media_tracker_front.fragments.HomeFragment;
+import com.northcoders.media_tracker_front.fragments.StatsFragment;
+import com.northcoders.media_tracker_front.fragments.WatchedFragment;
 
 public class MainActivity extends AppCompatActivity implements NavigationBarView.OnItemSelectedListener {
     NavigationBarView navigationBarView;
     HomeFragment homeFragment = new HomeFragment();
     CurrentlyWatchingFragment currentlyWatchingFragment = new CurrentlyWatchingFragment();
     BookmarkedFragment bookmarkedFragment = new BookmarkedFragment();
+    StatsFragment statsFragment = new StatsFragment();
+    WatchedFragment watchedFragment = new WatchedFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,6 +55,21 @@ public class MainActivity extends AppCompatActivity implements NavigationBarView
                     .commit();
             return true;
         }
+        if(item.getItemId() == R.id.stats){
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.frameLayoutFragment, statsFragment)
+                    .commit();
+            return true;
+        }
+        if(item.getItemId() == R.id.watched){
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.frameLayoutFragment,watchedFragment)
+                    .commit();
+            return true;
+        }
+
 
 
         return false;
