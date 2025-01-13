645560a7a03af70d992eef55d4b27c9364cf61c6
Shamar <Shamar098@gmail.com>
2025-01-13 15:22:59 +0000

    feat: implement navigation bar into main

--- app/src/main/java/com/northcoders/media_tracker_front/MainActivity.java
+++ app/src/main/java/com/northcoders/media_tracker_front/MainActivity.java
@@ -1,24 +1,40 @@
 package com.northcoders.media_tracker_front;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationBarView;
+import com.northcoders.media_tracker_front.fragments.HomeFragment;
 
+public class MainActivity extends AppCompatActivity implements NavigationBarView.OnItemSelectedListener {
+    NavigationBarView navigationBarView;
+    HomeFragment homeFragment = new HomeFragment();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        navigationBarView = findViewById(R.id.BottomNavView);
+        navigationBarView.setOnItemSelectedListener(this);
+        navigationBarView.setSelectedItemId(R.id.home);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.home){
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.frameLayoutFragment, homeFragment)
+                    .commit();
+            return true;
+        }
+        return false;
     }
 }
