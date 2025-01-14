55d8b425faa1afbbae770447fb35c83183576f16
Zoltan Kepes <zoltan.the.kepes@gmail.com>
2025-01-14 20:33:24 +0000

    feat(RetrofitInstance): code for the URL connection and data fetching

--- app/src/main/java/com/northcoders/media_tracker_front/service/RetrofitInstance.java
+++ app/src/main/java/com/northcoders/media_tracker_front/service/RetrofitInstance.java
@@ -1,4 +1,25 @@
 package com.northcoders.media_tracker_front.service;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class RetrofitInstance {
+    private static Retrofit retrofit = null;
+    private static final String baseURL = "http://10.0.2.2:8080/api/v1/recordShop/";
+
+    public static MovieApiService getService(){
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
+        if(retrofit == null){
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(baseURL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .client(client)
+                    .build();
+        }
+        return retrofit.create(MovieApiService.class);
+    }
 }
