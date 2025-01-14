384690752496b46ae359872c5f387e5a9606c648
Zoltan Kepes <zoltan.the.kepes@gmail.com>
2025-01-14 20:30:39 +0000

    feat(MovieApiService): api service interface for the 'history' get request

--- app/src/main/java/com/northcoders/media_tracker_front/service/MovieApiService.java
+++ app/src/main/java/com/northcoders/media_tracker_front/service/MovieApiService.java
@@ -1,4 +1,11 @@
 package com.northcoders.media_tracker_front.service;
 
-public class MovieApiService {
+import com.northcoders.media_tracker_front.model.WatchHistory;
+import java.util.List;
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface MovieApiService {
+    @GET("history")
+    Call<List<WatchHistory>> getHistory();
 }
