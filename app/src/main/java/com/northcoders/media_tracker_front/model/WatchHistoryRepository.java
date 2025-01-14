54289514f2e57c0b855367b4a6765ed56fd878d5
Zoltan Kepes <zoltan.the.kepes@gmail.com>
2025-01-14 20:28:10 +0000

    feat(WatchHistoryRepository): add repository class for the watch history class

---
+++ app/src/main/java/com/northcoders/media_tracker_front/model/WatchHistoryRepository.java
@@ -0,0 +1,40 @@
+package com.northcoders.media_tracker_front.model;
+
+import android.app.Application;
+import android.util.Log;
+import androidx.lifecycle.MutableLiveData;
+import com.northcoders.media_tracker_front.service.MovieApiService;
+import com.northcoders.media_tracker_front.service.RetrofitInstance;
+import java.util.List;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class WatchHistoryRepository {
+    private MutableLiveData<List<WatchHistory>> mutableLiveData = new MutableLiveData<>();
+    private Application application;
+
+    public WatchHistoryRepository(Application application) {
+        this.application = application;
+    }
+
+    public MutableLiveData<List<WatchHistory>> getMutableLiveData(){
+        MovieApiService movieApiService = RetrofitInstance.getService();
+
+        // getHistory() fetches the data, see 'service'
+        Call<List<WatchHistory>> call = movieApiService.getHistory();
+        call.enqueue(new Callback<List<WatchHistory>>(){
+            @Override
+            public void onResponse(Call<List<WatchHistory>> call, Response<List<WatchHistory>> response) {
+                List<WatchHistory> albums = response.body();
+                mutableLiveData.setValue(albums);
+            }
+
+            @Override
+            public void onFailure(Call<List<WatchHistory>> call, Throwable t) {
+                Log.i("GET request", t.getMessage());
+            }
+        });
+        return mutableLiveData;
+    }
+}
---
+++ app/src/main/java/com/northcoders/media_tracker_front/service/MovieApiService.java
@@ -0,0 +1,4 @@
+package com.northcoders.media_tracker_front.service;
+
+public class MovieApiService {
+}
