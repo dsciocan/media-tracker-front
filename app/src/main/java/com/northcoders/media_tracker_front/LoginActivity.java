01076ea4f16bae0b85cbecb651a99444886fce1f
Shamar <Shamar098@gmail.com>
2025-01-15 11:57:42 +0000

    feat: add login activity/layout

---
+++ app/src/main/java/com/northcoders/media_tracker_front/LoginActivity.java
@@ -0,0 +1,19 @@
+package com.northcoders.media_tracker_front;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+
+    }
+
+
+}
