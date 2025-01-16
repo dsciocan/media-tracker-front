fd849e334758fbe2b38466c33265395eaf05f1ed
Shamar <Shamar098@gmail.com>
2025-01-16 23:11:39 +0000

    feat: add sample User class

---
+++ app/src/main/java/com/northcoders/media_tracker_front/model/User.java
@@ -0,0 +1,12 @@
+package com.northcoders.media_tracker_front.model;
+
+public class User {
+    String email ;
+    boolean emailVerified;
+    String name;
+    String pictureUrl;
+    String locale;
+    String familyName;
+    String givenName;
+
+}
