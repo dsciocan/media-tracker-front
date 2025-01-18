7583de8e43e55d78afbc71738d2ec6b0bf9a50a5
lexadecimals <alexis.wilkinsondefoe@gmail.com>
2025-01-18 15:06:38 +0000

    fix: delete changes to baseurl

--- app/src/main/java/com/northcoders/media_tracker_front/service/RetrofitInstance.java
+++ app/src/main/java/com/northcoders/media_tracker_front/service/RetrofitInstance.java
@@ -7,7 +7,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitInstance {
     private static Retrofit retrofit = null;
-    private static final String baseURL = "http://10.0.2.2:8080/api/v1/mediatracker/films";
+    private static final String baseURL = "http://10.0.2.2:8080/api/v1";
 
     public static MovieApiService getService(){
         HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
