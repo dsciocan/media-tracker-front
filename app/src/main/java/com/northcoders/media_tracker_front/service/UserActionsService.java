11514442deb1dc919fd56df67cf3a01b9d291979
Shamar <Shamar098@gmail.com>
2025-01-18 05:35:52 +0000

    feat: add GET request auth

--- app/src/main/java/com/northcoders/media_tracker_front/service/UserActionsService.java
+++ app/src/main/java/com/northcoders/media_tracker_front/service/UserActionsService.java
@@ -1,13 +1,10 @@
 package com.northcoders.media_tracker_front.service;
 
-import com.northcoders.media_tracker_front.model.User;
-
 import retrofit2.Call;
-import retrofit2.http.POST;
+import retrofit2.http.GET;
 public interface UserActionsService {
 
-    // Example request -> This one won't be included in the final product
-    @POST("users/auth")
-    Call<User> authUser();
 
+    @GET("users/auth")
+    Call<Void> auth();
 }
