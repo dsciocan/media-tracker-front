685568b4b0b36fc13a4c13250a28f6320645da48
Shamar <Shamar098@gmail.com>
2025-01-19 03:38:28 +0000

    refactor: clean up imports

--- app/src/main/java/com/northcoders/media_tracker_front/LoginActivity.java
+++ app/src/main/java/com/northcoders/media_tracker_front/LoginActivity.java
@@ -17,10 +17,8 @@ import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
 import com.firebase.ui.auth.IdpResponse;
 import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GetTokenResult;
 
 import java.util.Arrays;
 import java.util.List;
