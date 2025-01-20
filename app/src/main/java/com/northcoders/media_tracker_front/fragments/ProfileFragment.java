075057628003fd31740acd742b1a33a1ae95bcc7
Shamar <Shamar098@gmail.com>
2025-01-20 02:20:04 +0000

    feat: add profile picture to profile page

--- app/src/main/java/com/northcoders/media_tracker_front/fragments/ProfileFragment.java
+++ app/src/main/java/com/northcoders/media_tracker_front/fragments/ProfileFragment.java
@@ -10,6 +10,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.bumptech.glide.Glide;
+import com.google.android.material.imageview.ShapeableImageView;
+import com.google.firebase.auth.FirebaseAuth;
 import com.northcoders.media_tracker_front.R;
 import com.northcoders.media_tracker_front.databinding.FragmentProfileBinding;
 
@@ -20,6 +23,19 @@ public class ProfileFragment extends Fragment {
         // Required empty public constructor
     }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Load in the user's profile picture from the google account
+        // As a circular image
+        ShapeableImageView profilePicture = binding.profileFragmentAvatar;
+        Glide.with(profilePicture)
+                .load(FirebaseAuth.getInstance().getCurrentUser().getPhotoUrl().toString())
+                .circleCrop()
+                .error(R.drawable.circularcustombutton)
+                .into(profilePicture);
+    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
