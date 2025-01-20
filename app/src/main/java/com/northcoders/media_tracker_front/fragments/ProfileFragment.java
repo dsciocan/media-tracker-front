57ad91075f990599f28db5cf8efaf095ce7b48ce
Shamar <Shamar098@gmail.com>
2025-01-20 02:07:38 +0000

    feat: add binding to ProfileFragment

--- app/src/main/java/com/northcoders/media_tracker_front/fragments/ProfileFragment.java
+++ app/src/main/java/com/northcoders/media_tracker_front/fragments/ProfileFragment.java
@@ -2,6 +2,8 @@ package com.northcoders.media_tracker_front.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -9,8 +11,10 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.northcoders.media_tracker_front.R;
+import com.northcoders.media_tracker_front.databinding.FragmentProfileBinding;
 
 public class ProfileFragment extends Fragment {
+    private FragmentProfileBinding binding;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -20,6 +24,7 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_profile,container,false);
+        return binding.getRoot();
     }
 }
