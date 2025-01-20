6a9f11d952bcb58fa35e7b034b81ca29dfbe0f10
Shamar <Shamar098@gmail.com>
2025-01-20 14:28:30 +0000

    feat: add Delete call to service and AppUserRepository

--- app/src/main/java/com/northcoders/media_tracker_front/service/UserActionsService.java
+++ app/src/main/java/com/northcoders/media_tracker_front/service/UserActionsService.java
@@ -1,10 +1,13 @@
 package com.northcoders.media_tracker_front.service;
 
 import retrofit2.Call;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 public interface UserActionsService {
 
-
     @GET("users/auth")
     Call<Void> auth();
+
+    @DELETE("users")
+    Call<Void> deleteUser();
 }
