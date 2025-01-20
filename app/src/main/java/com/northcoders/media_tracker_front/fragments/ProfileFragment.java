7d27c9a0f4ab1fc7f73e95affd3853f91e7f30b2
Shamar <Shamar098@gmail.com>
2025-01-20 14:30:28 +0000

    feat: add delete button logic to profile fragment

--- app/src/main/java/com/northcoders/media_tracker_front/fragments/ProfileFragment.java
+++ app/src/main/java/com/northcoders/media_tracker_front/fragments/ProfileFragment.java
@@ -1,13 +1,16 @@
 package com.northcoders.media_tracker_front.fragments;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,8 +22,10 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.imageview.ShapeableImageView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.northcoders.media_tracker_front.R;
 import com.northcoders.media_tracker_front.databinding.FragmentProfileBinding;
+import com.northcoders.media_tracker_front.viewmodel.ProfileViewModel;
 
 public class ProfileFragment extends Fragment {
     private FragmentProfileBinding binding;
@@ -32,9 +37,12 @@ public class ProfileFragment extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        // These should be in a clickHandler class
+        // No time at this point
         loadProfilePicture();
         loadLogoutButton();
-
+        loadDeleteButton();
 
 
     }
@@ -80,6 +88,71 @@ public class ProfileFragment extends Fragment {
         });
 
     }
+
+    private void loadDeleteButton() {
+        ProfileViewModel profileViewModel = new ProfileViewModel(this.requireActivity().getApplication());
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+    binding.profileFragmentDeleteButton.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            AlertDialog.Builder quit = new AlertDialog.Builder(getContext())
+                    .setTitle("Delete Account")
+                    .setMessage("Are you sure you want to delete your account " +user.getDisplayName() +"\"?")
+                    .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            Log.i("Profile Fragment","USER PRESSED YES");
+                            profileViewModel.deleteUser();
+                            // For now the user will just be locally signed out
+                            // Their data on the backend will be deleted but not their firebase account
+                            // When they log in again another user entry will be made on the backend
+                            FirebaseAuth.getInstance().signOut();
+                            //deleteUserFirebase();
+                            Toast.makeText(getContext(), "You have successfully deleted your account", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(getContext(),com.northcoders.media_tracker_front.LoginActivity.class);
+                            getActivity().startActivity(intent);
+                            // finishing the activity prevents the back button opening the app again
+                            getActivity().finish();
+                        }
+                    })
+                    .setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.cancel();
+                        }
+                    });
+            quit.show();
+        }
+
+    });
+    }
+
+
+    /**
+     * This is the method to delete the user from Firebase
+     * Will be done in the future but can't be done at this time
+     * For now the user will be deleted off the backend database
+     */
+
+   /* private void deleteUserFirebase(){
+
+        FirebaseAuth.getInstance().getCurrentUser().delete().addOnSuccessListener(
+                new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        Log.d("ProfileFragment", "User account deleted.");
+
+                        Toast.makeText(getContext(), "You have successfully deleted your account", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(getContext(),com.northcoders.media_tracker_front.LoginActivity.class);
+                        getActivity().startActivity(intent);
+                        // finishing the activity prevents the back button opening the app again
+                        getActivity().finish();
+
+                    }
+                }
+        );
+
+    }*/
 
 
 
