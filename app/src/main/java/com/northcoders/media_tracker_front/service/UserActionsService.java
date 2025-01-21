c8883406f9d764f46418e9fe084d27aec0a03452
Stef Daniela Ciocan <daniela.ciocan137@gmail.com>
2025-01-21 19:07:16 +0000

    move movieApiService user specific methods to UserActionsService

--- app/src/main/java/com/northcoders/media_tracker_front/service/UserActionsService.java
+++ app/src/main/java/com/northcoders/media_tracker_front/service/UserActionsService.java
@@ -1,5 +1,6 @@
 package com.northcoders.media_tracker_front.service;
 
+import com.northcoders.media_tracker_front.model.Bookmarked;
 import com.northcoders.media_tracker_front.model.WatchHistory;
 
 import java.util.List;
@@ -7,6 +8,7 @@ import java.util.List;
 import retrofit2.Call;
 import retrofit2.http.DELETE;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface UserActionsService {
@@ -16,6 +18,13 @@ public interface UserActionsService {
 
     @GET("users/films/search")
     Call<List<WatchHistory>> getHistory(@Query("status") String status);
+
+    @GET("users/films/search?status=BOOKMARKED")
+    Call<List<Bookmarked>> getBookmarked();
+
+    @GET("users/films/{filmId}")
+    Call<Bookmarked> getBookmarkedFilm(@Path("filmId") Long filmId);
+
     @DELETE("users")
     Call<Void> deleteUser();
 }
