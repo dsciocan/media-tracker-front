4314f6cca10cacd5534c5cd47a3b3edae599db0e
Stef Daniela Ciocan <daniela.ciocan137@gmail.com>
2025-01-21 19:09:13 +0000

    feat: create BookmarkedDetailsFragment

---
+++ app/src/main/java/com/northcoders/media_tracker_front/fragments/BookmarkedDetailsFragment.java
@@ -0,0 +1,107 @@
+package com.northcoders.media_tracker_front.fragments;
+
+import android.os.Bundle;
+
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+
+import com.bumptech.glide.Glide;
+import com.google.firebase.auth.FirebaseAuth;
+import com.northcoders.media_tracker_front.R;
+import com.northcoders.media_tracker_front.adapter.BookmarkedAdapter;
+import com.northcoders.media_tracker_front.databinding.FragmentBookmarkedDetailsBinding;
+import com.northcoders.media_tracker_front.model.Bookmarked;
+import com.northcoders.media_tracker_front.viewmodel.BookmarkedDetailsViewModel;
+import com.northcoders.media_tracker_front.viewmodel.BookmarkedViewModel;
+
+import org.jetbrains.annotations.Nullable;
+
+
+public class BookmarkedDetailsFragment extends Fragment {
+    BookmarkedDetailsViewModel bookmarkedDetailsViewModel;
+   Bookmarked bookmarked;
+   FragmentBookmarkedDetailsBinding binding;
+   ProfileFragment profileFragment = new ProfileFragment();
+    private static final String MOVIE_ID_KEY = "movieKey"  ;
+
+
+    public BookmarkedDetailsFragment() {
+        // Required empty public constructor
+    }
+
+
+
+
+
+    public static BookmarkedDetailsFragment newInstance(Long id){
+        BookmarkedDetailsFragment movieFragment = new BookmarkedDetailsFragment();
+        Bundle bundle = new Bundle();
+        bundle.putLong(MOVIE_ID_KEY,id);
+        movieFragment.setArguments(bundle);
+        return movieFragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        bookmarkedDetailsViewModel = new ViewModelProvider(this).get(BookmarkedDetailsViewModel.class);
+    }
+
+        @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            Long filmId = getArguments().getLong("movieKey");
+            bookmarkedDetailsViewModel.getUserFilm(filmId).getValue();
+            bookmarked = bookmarkedDetailsViewModel.getUserFilm(filmId).getValue();
+            binding.setBookmarked(bookmarked);
+        }
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_bookmarked_details, container, false);
+        // Inflate the layout for this fragment
+        ImageButton profilePicture = binding.profilepicturebookmarked;
+        Glide.with(profilePicture)
+                .load(FirebaseAuth.getInstance().getCurrentUser().getPhotoUrl().toString())
+                .circleCrop()
+//                .apply(RequestOptions.circleCropTransform())
+                .error(R.drawable.circularcustombutton)
+                .into(profilePicture);
+
+
+        profilePicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager()
+                        .beginTransaction()
+                        .setCustomAnimations(
+                                android.R.anim.fade_in,
+                                android.R.anim.fade_out,
+                                android.R.anim.slide_in_left,
+                                android.R.anim.slide_out_right)
+                        .replace(R.id.frameLayoutFragment, profileFragment)
+                        .commit();
+            }
+        });
+
+    return binding.getRoot();
+
+
+//        Glide.with(this).load(bookmarked.getUserFilmId().getFilm().getPoster_url()).into(binding.bookmarkedFragmentImage);
+
+    }
+
+}
