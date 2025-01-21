84a5f9224d2bd59ff8c77f9b9d7eeb6cec1fcb67
Shamar <Shamar098@gmail.com>
2025-01-21 00:34:29 +0000

    feat: add logic for film search call to view model

--- app/src/main/java/com/northcoders/media_tracker_front/viewmodel/FilmSearchResultViewModel.java
+++ app/src/main/java/com/northcoders/media_tracker_front/viewmodel/FilmSearchResultViewModel.java
@@ -1,4 +1,26 @@
 package com.northcoders.media_tracker_front.viewmodel;
 
-public class FilmSearchResultViewModel {
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.MutableLiveData;
+
+import com.northcoders.media_tracker_front.model.FilmSearchResult;
+import com.northcoders.media_tracker_front.model.FilmSearchResultRepository;
+
+import java.util.List;
+
+public class FilmSearchResultViewModel extends AndroidViewModel {
+FilmSearchResultRepository filmSearchResultRepository;
+
+
+    public FilmSearchResultViewModel(@NonNull Application application) {
+        super(application);
+        this.filmSearchResultRepository = new FilmSearchResultRepository(application);
+    }
+
+    public MutableLiveData<List<FilmSearchResult>> getFilmSearchResults(String searchQuery){
+        return filmSearchResultRepository.getMutableLiveData(searchQuery);
+    }
 }
