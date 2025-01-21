4affa14f81aeefdafaac0f854392a887555826c4
Sean Winston <seanwinston@gmail.com>
2025-01-21 23:44:50 +0000

    feat: implement logic to get current film and binding in displayInPage method

--- app/src/main/java/com/northcoders/media_tracker_front/fragments/WatchedMovieFragment.java
+++ app/src/main/java/com/northcoders/media_tracker_front/fragments/WatchedMovieFragment.java
@@ -3,18 +3,25 @@ package com.northcoders.media_tracker_front.fragments;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.northcoders.media_tracker_front.R;
 import com.northcoders.media_tracker_front.databinding.FragmentWatchedBinding;
+import com.northcoders.media_tracker_front.databinding.FragmentWatchedMovieBinding;
 import com.northcoders.media_tracker_front.model.FilmDetails;
 import com.northcoders.media_tracker_front.model.WatchHistory;
 import com.northcoders.media_tracker_front.viewmodel.WatchHistoryViewModel;
+
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -25,10 +32,10 @@ public class WatchedMovieFragment extends Fragment {
 
     FilmDetails currentFilmDetails;
 
-    WatchHistory currentFilm;
+    WatchHistory currentFilm = new WatchHistory();
 
     WatchHistoryViewModel viewModel;
-    FragmentWatchedBinding binding;
+    FragmentWatchedMovieBinding binding;
 
     private final static String MOVIE_ID_KEY = "moviekey";
 
@@ -50,6 +57,7 @@ public class WatchedMovieFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        viewModel = new ViewModelProvider(this).get(WatchHistoryViewModel.class) ;
 
     }
 
@@ -57,8 +65,7 @@ public class WatchedMovieFragment extends Fragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstance){
         super.onCreate(savedInstance);
         if (getArguments() != null) {
-            getArguments().getLong(MOVIE_ID_KEY);
-            viewModel.getWatchHistory();
+            getUserFilmDetails(getArguments().getLong(MOVIE_ID_KEY));
         }
     }
 
@@ -66,8 +73,8 @@ public class WatchedMovieFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_watched_movie, container, false);
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_watched_movie, container, false );
+        return binding.getRoot();
     }
 
     public void getUserFilmDetails(Long id){
@@ -75,13 +82,28 @@ public class WatchedMovieFragment extends Fragment {
             @Override
             public void onChanged(WatchHistory watchHistory) {
                 currentFilm = watchHistory;
-                displayInPage();
+                displayInPage(currentFilm);
             }
         });
     }
 
-    public void displayInPage(){
-        binding.watchHistoryTitle.setText(currentFilm.getUserFilmId().getFilm().getTitle());
+    public void displayInPage(WatchHistory watchHistory){
+        binding.movieFragmentTitle.setText(watchHistory.getUserFilmId().getFilm().getTitle());
+        binding.movieYear.setText(watchHistory.getWatchedDate());
+        binding.movieRuntime.setText(watchHistory.getUserFilmId().getFilm().getDuration() + " mins");
+        binding.movieLanguage.setText(watchHistory.getUserFilmId().getFilm().getLanguage());
+        binding.movieCountry.setText(watchHistory.getUserFilmId().getFilm().getCountry());
+
+        Glide.with(binding.movieFragmentImage.getContext())
+                .load(watchHistory.getUserFilmId().getFilm().getPoster_url())
+                .into(binding.movieFragmentImage);
+        List<String> genres = watchHistory.getUserFilmId().getFilm().getGenres();
+        binding.movieGenres.setText(String.join(", ", genres));
+        TextView detailsTextView = binding.movieFragmentDetailsCard.findViewById(R.id.details_text_view);
+        detailsTextView.setText(watchHistory.getUserFilmId().getFilm().getSynopsis());
+        binding.movieFragmentNotesInput.setText(watchHistory.getNotes());
+        int rating = watchHistory.getRating();
+        binding.movieFragmentRatingBar.setRating(rating);
 
     }
 
