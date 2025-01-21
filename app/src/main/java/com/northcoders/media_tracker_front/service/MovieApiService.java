99c6560b82b2577c68ea3675d2141dca73a83f8a
Stef Daniela Ciocan <daniela.ciocan137@gmail.com>
2025-01-21 16:01:59 +0000

    add getBookmarkedFilm method to MovieApiService

--- app/src/main/java/com/northcoders/media_tracker_front/service/MovieApiService.java
+++ app/src/main/java/com/northcoders/media_tracker_front/service/MovieApiService.java
@@ -3,7 +3,6 @@ package com.northcoders.media_tracker_front.service;
 import com.northcoders.media_tracker_front.model.FilmSearchResult;
 import com.northcoders.media_tracker_front.model.ShowSearchResult;
 import com.northcoders.media_tracker_front.model.Bookmarked;
-import com.northcoders.media_tracker_front.model.WatchHistory;
 import com.northcoders.media_tracker_front.model.Watching;
 import java.util.List;
 import retrofit2.Call;
@@ -20,6 +19,9 @@ public interface MovieApiService {
 
     @GET("watching")
     Call<List<Watching>> getWatching();
+
+    @GET("users/films/{filmId}")
+    Call<Bookmarked> getBookmarkedFilm(@Path("filmId") Long filmId);
 
     @GET("films/search/{query}")
     Call<List<FilmSearchResult>> getFilmSearchResults(@Path("query") String searchQuery);
