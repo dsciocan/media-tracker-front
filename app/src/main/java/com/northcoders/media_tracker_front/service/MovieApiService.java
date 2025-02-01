c0ff45a4375898a6eb32cb382e1c6c5290cb4150
Stef Daniela Ciocan <daniela.ciocan137@gmail.com>
2025-02-01 18:23:53 +0000

    feat: add getFilmByTmdbId api call method

--- app/src/main/java/com/northcoders/media_tracker_front/service/MovieApiService.java
+++ app/src/main/java/com/northcoders/media_tracker_front/service/MovieApiService.java
@@ -1,10 +1,9 @@
 package com.northcoders.media_tracker_front.service;
 
+import com.northcoders.media_tracker_front.model.Film;
 import com.northcoders.media_tracker_front.model.FilmDetails;
 import com.northcoders.media_tracker_front.model.FilmSearchResult;
 import com.northcoders.media_tracker_front.model.ShowSearchResult;
-import com.northcoders.media_tracker_front.model.Bookmarked;
-import com.northcoders.media_tracker_front.model.WatchHistory;
 import com.northcoders.media_tracker_front.model.Watching;
 import java.util.List;
 import retrofit2.Call;
@@ -28,4 +27,9 @@ public interface MovieApiService {
 
     @GET("films/details/{movieId}")
     Call<FilmDetails> getFilmDetails(@Path("movieId") Long movieId);
+
+    @GET("films/saved/tmdb={tmdbId}")
+    Call<Film> getFilmByTmdbId(@Path("tmdbId") Long tmdbId);
+
+
 }
