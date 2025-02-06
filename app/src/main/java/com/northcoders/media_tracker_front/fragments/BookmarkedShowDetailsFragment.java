da714a61b92e47e260500fb78bf4c9fb6b813a37
Stef Daniela Ciocan <daniela.ciocan137@gmail.com>
2025-02-06 21:13:41 +0000

    feat: create ShowDetails and BookmarkedShowDetails fragments similar to Film ones

--- app/src/main/java/com/northcoders/media_tracker_front/fragments/BookmarkedShowDetailsFragment.java
+++ app/src/main/java/com/northcoders/media_tracker_front/fragments/BookmarkedShowDetailsFragment.java
@@ -1,14 +1,36 @@
 package com.northcoders.media_tracker_front.fragments;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AlertDialog;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.google.firebase.auth.FirebaseAuth;
 import com.northcoders.media_tracker_front.R;
+import com.northcoders.media_tracker_front.databinding.FragmentBookmarkedDetailsBinding;
+import com.northcoders.media_tracker_front.databinding.FragmentBookmarkedShowDetailsBinding;
+import com.northcoders.media_tracker_front.model.UserFilm;
+import com.northcoders.media_tracker_front.model.UserShow;
+import com.northcoders.media_tracker_front.viewmodel.BookmarkedDetailsViewModel;
+
+import org.jetbrains.annotations.Nullable;
+
+import java.time.LocalDate;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,50 +39,246 @@ import com.northcoders.media_tracker_front.R;
  */
 public class BookmarkedShowDetailsFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    BookmarkedDetailsViewModel bookmarkedDetailsViewModel;
+    UserShow userShowShow = new UserShow();
+    UserShow currentShowDetails;
+    FragmentBookmarkedShowDetailsBinding binding;
+    ProfileFragment profileFragment = new ProfileFragment();
+    BookmarkedFragment bookmarkedFragment = new BookmarkedFragment();
+    private static final String MOVIE_ID_KEY = "movieKey"  ;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public BookmarkedShowDetailsFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment BookmarkedShowDetailsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static BookmarkedShowDetailsFragment newInstance(String param1, String param2) {
-        BookmarkedShowDetailsFragment fragment = new BookmarkedShowDetailsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+
+    public static BookmarkedShowDetailsFragment newInstance(Long id){
+        BookmarkedShowDetailsFragment movieFragment = new BookmarkedShowDetailsFragment();
+        Log.i("INSTANCE", String.valueOf(id));
+        Bundle bundle = new Bundle();
+        bundle.putLong(MOVIE_ID_KEY,id);
+        movieFragment.setArguments(bundle);
+        return movieFragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        bookmarkedDetailsViewModel = new ViewModelProvider(this).get(BookmarkedDetailsViewModel.class);
     }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            Long showId = getArguments().getLong("movieKey");
+            getShowDetails(showId);
+        }
+        loadProfilePicture();
+        backButtonLogic();
+        setDeleteButtonLogic();
+        editTextViewLogic();
+        watchedButton();
+        setSaveButtonLogic();
+
+    }
+
+    public void getShowDetails(Long id) {
+        bookmarkedDetailsViewModel.getUserShow(id).observe(getViewLifecycleOwner(), new Observer<UserShow>() {
+            @Override
+            public void onChanged(UserShow userShow) {
+                userShowShow = userShow;
+                setBindingText(userShowShow);
+            }
+        });
+    }
+
+    public void setBindingText(UserShow userShow) {
+        Log.i("BOOKMARKEDDETAILSFRAGMENT", userShow.getUserShowId().getShow().getTitle());
+        binding.setUserShow(userShow);
+        binding.bookmarkedFragmentRatingBar.setRating(userShow.getRating());
+//        binding.bookmarkedFragmentTitle.setText(bookmarked.getUserShowId().getShow().getTitle());
+        Glide.with(this).load(userShow.getUserShowId().getShow().getPosterUrl()).into(binding.bookmarkedFragmentImage);
+    }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_bookmarked_show_details, container, false);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_bookmarked_show_details, container, false);
+        return binding.getRoot();
+
+    }
+
+
+    public void setDeleteButtonLogic() {
+        binding.bookmarkedFragmentBookmarkBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!binding.bookmarkedFragmentBookmarkBtn.isChecked()) {
+                    deleteAlert();
+                }
+            }
+        });
+    }
+
+    private void deleteAlert() {
+        AlertDialog.Builder quit = new AlertDialog.Builder(getContext())
+                .setTitle("Remove From Bookmarks")
+                .setMessage("Are you sure you want to remove this from your 'bookmarked' list?")
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Log.i("Watched Movie Fragment", "USER CONFIRMED DELETE");
+                        // Backend only has call to delete user show -> not necessarily by status
+                        // Same logic applied for both watched and bookmarked
+//                        bookmarkedDetailsViewModel.deleteUserShow(userShowShow.getUserShowId().getShow().getId());
+                        Toast.makeText(getContext(), "Successfully Removed", Toast.LENGTH_SHORT).show();
+                        dialog.dismiss();
+                        getActivity().getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.frameLayoutFragment, bookmarkedFragment)
+                                .commit();
+                    }
+                })
+                .setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        binding.bookmarkedFragmentBookmarkBtn.setChecked(true);
+                        dialog.cancel();
+                    }
+                });
+        quit.show();
+    }
+
+
+
+    public void setSaveButtonLogic() {
+        binding.bookmarkedSaveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editTextDialog();
+            }
+        });
+    }
+    private void editTextViewLogic(){
+        binding.bookmarkedFragmentNotesInput.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE ||
+                        (event != null && event.getKeyCode() == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_DOWN)) {
+                    editTextDialog();
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    public void editTextDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setTitle("Update Movie")
+                .setMessage("Would you like to apply these changes?")
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        currentShowDetails = userShowShow;
+                        String inputText = binding.bookmarkedFragmentNotesInput.getText().toString();
+                        int rating = (int) binding.bookmarkedFragmentRatingBar.getRating();
+                        currentShowDetails.setNotes(inputText);
+                        currentShowDetails.setRating(rating);
+                        bookmarkedDetailsViewModel.updateUserShow(currentShowDetails.getUserShowId().getShow().getId(),currentShowDetails);
+                        dialog.dismiss();
+                        Toast.makeText(getContext(), "Movie Updated", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+        builder.show();
+    }
+
+    public void loadProfilePicture(){
+        ImageButton profilePicture = binding.profilepicturebookmarked;
+        Glide.with(profilePicture)
+                .load(FirebaseAuth.getInstance().getCurrentUser().getPhotoUrl().toString())
+                .circleCrop()
+//                .apply(RequestOptions.circleCropTransform())
+                .error(R.drawable.circularcustombutton)
+                .into(profilePicture);
+
+
+        profilePicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager()
+                        .beginTransaction()
+                        .setCustomAnimations(
+                                android.R.anim.fade_in,
+                                android.R.anim.fade_out,
+                                android.R.anim.slide_in_left,
+                                android.R.anim.slide_out_right)
+                        .replace(R.id.frameLayoutFragment, profileFragment)
+                        .addToBackStack("BookmarkedShowDetailsTransaction")
+                        .commit();
+            }
+        });
+
+    }
+
+    public void backButtonLogic(){
+        binding.bookmarkedFragmentBackFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.frameLayoutFragment, bookmarkedFragment)
+                        .commit();
+            }
+        });
+    }
+
+    public void watchedButton() {
+        binding.bookmarkedToWatchedButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                moveAlert();
+            }
+        });
+    }
+
+
+    private void moveAlert() {
+        AlertDialog.Builder quit = new AlertDialog.Builder(getContext())
+                .setTitle("Move to Watched")
+                .setMessage("Are you sure you want to move this to your 'watched' list?")
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Log.i("Watched Movie Fragment", "USER CONFIRMED DELETE");
+                        // Backend only has call to delete user show -> not necessarily by status
+                        // Same logic applied for both watched and bookmarked
+                        UserShow userShow = binding.getUserShow();
+                        userShow.setStatus("WATCHED");
+                        userShow.setWatchedDate(LocalDate.now().toString());
+                        bookmarkedDetailsViewModel.updateUserShow(userShow.getUserShowId().getShow().getId(), userShow);
+                        getActivity().getSupportFragmentManager()
+                                .beginTransaction()
+                                .replace(R.id.frameLayoutFragment, bookmarkedFragment)
+                                .commit();
+                    }
+                })
+                .setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+        quit.show();
     }
 }
--- app/src/main/java/com/northcoders/media_tracker_front/fragments/ShowDetailsFragment.java
+++ app/src/main/java/com/northcoders/media_tracker_front/fragments/ShowDetailsFragment.java
@@ -1,66 +1,323 @@
 package com.northcoders.media_tracker_front.fragments;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.RatingBar;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
 import com.northcoders.media_tracker_front.R;
+import com.northcoders.media_tracker_front.databinding.FragmentMovieBinding;
+import com.northcoders.media_tracker_front.databinding.FragmentShowDetailsBinding;
+import com.northcoders.media_tracker_front.model.Film;
+import com.northcoders.media_tracker_front.model.FilmDetails;
+import com.northcoders.media_tracker_front.model.Show;
+import com.northcoders.media_tracker_front.model.ShowDetails;
+import com.northcoders.media_tracker_front.model.UserFilm;
+import com.northcoders.media_tracker_front.model.UserShow;
+import com.northcoders.media_tracker_front.model.UserShowId;
+import com.northcoders.media_tracker_front.viewmodel.MovieDetailsViewModel;
+import com.northcoders.media_tracker_front.viewmodel.ShowDetailsViewModel;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ShowDetailsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.time.LocalDate;
+
+
 public class ShowDetailsFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private static final String SHOW_ID_KEY = "ShowKey";
+    ShowDetails showDetails = new ShowDetails();
+    ShowDetailsViewModel viewModel;
+    private ProfileFragment profileFragment = new ProfileFragment();
+    FragmentShowDetailsBinding binding;
+    UserShow userShow = new UserShow();
+    Show show = new Show();
+    Boolean isSaved = false;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+
 
     public ShowDetailsFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ShowDetailsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ShowDetailsFragment newInstance(String param1, String param2) {
+
+    public static ShowDetailsFragment newInstance(Long showId) {
         ShowDetailsFragment fragment = new ShowDetailsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
+        Bundle bundle = new Bundle();
+        bundle.putLong(SHOW_ID_KEY, showId);
+        fragment.setArguments(bundle);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        viewModel = new ViewModelProvider(this).get(ShowDetailsViewModel.class);
+
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            Long id = getArguments().getLong(SHOW_ID_KEY);
+            getShowDetails(id);
+            isShowSaved(id);
+            getShow(id);
+            getUserShow(show.getId());
         }
+        backButtonLogic();
+        handleShowStatusLogic();
+        setBookmarkLogic();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_show_details, container, false);
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_show_details, container, false);
+        return binding.getRoot();
     }
+
+
+    public void bindContent(ShowDetails showDetails){
+        binding.setShow(showDetails);
+        Glide.with(binding.poster.getContext())
+                .load(showDetails.getPoster_path())
+                .into(binding.poster);
+        ImageButton profilePicture = binding.avatar ;
+        Glide.with(profilePicture)
+                .load(FirebaseAuth.getInstance().getCurrentUser().getPhotoUrl().toString())
+                .circleCrop()
+                .error(R.drawable.circularcustombutton)
+                .into(profilePicture);
+        profilePicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager()
+                        .beginTransaction()
+                        .setCustomAnimations(
+                                android.R.anim.fade_in,
+                                android.R.anim.fade_out,
+                                android.R.anim.slide_in_left,
+                                android.R.anim.slide_out_right)
+                        .replace(R.id.frameLayoutFragment, profileFragment)
+                        .addToBackStack("profileFragmentTransaction") // allow user to press back to go back to home fragment
+                        .commit();
+            }
+        });
+    }
+
+    public void setBookmarkLogic(){
+        binding.bookmark.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(checkShowStatus().equals("WATCHED")) {
+                    Toast.makeText(getContext(), "Show already watched", Toast.LENGTH_SHORT).show();
+                    binding.bookmark.setChecked(false);
+                } else if (checkShowStatus().equals("BOOKMARKED") && binding.bookmark.isChecked()) {
+                    Toast.makeText(getContext(), "Show already bookmarked", Toast.LENGTH_SHORT).show();
+                    binding.bookmark.setChecked(true);
+                } else if(binding.bookmark.isChecked()) {
+                    switchAlert();
+                } else {
+                    removeAlert();
+                }
+            }
+        });
+
+    }
+
+    private void switchAlert(){
+        AlertDialog.Builder quit = new AlertDialog.Builder(getContext())
+                .setTitle("Add To Bookmark")
+                .setMessage("Do you want to add this to your 'Bookmarked' list? ")
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        UserShow savedShow = new UserShow();
+                        savedShow.setStatus("BOOKMARKED");
+                        viewModel.saveUserShow(getArguments().getLong(SHOW_ID_KEY), savedShow);
+                        getShow(getArguments().getLong(SHOW_ID_KEY));
+                        Toast.makeText(getContext(), "Successfully Added To Bookmarked", Toast.LENGTH_SHORT).show();
+                        getUserShow(show.getId());
+                    }
+                })
+                .setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        binding.bookmark.setChecked(false);
+                        dialog.cancel();
+                    }
+                });
+        quit.show();
+    }
+
+
+    public void removeAlert() {
+        AlertDialog.Builder quit = new AlertDialog.Builder(getContext())
+                .setTitle("Remove From Bookmarks")
+                .setMessage("Do you want to remove this from your 'Bookmarked' list? ")
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        //TODO WIP DONT FORGET
+                        getShow(getArguments().getLong(SHOW_ID_KEY));
+                        Show savedShow = show;
+//                        viewModel.deleteUserFilm(savedFilm.getId());
+                        Toast.makeText(getContext(), "Successfully Removed From Bookmarked", Toast.LENGTH_SHORT).show();
+
+                    }
+                })
+                .setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        binding.bookmark.setChecked(true);
+                        dialog.cancel();
+                    }
+                });
+        quit.show();
+    }
+
+    private void getShow(Long tmdbId) {
+        viewModel.getShowDetailsByTmdbId(tmdbId).observe(getViewLifecycleOwner(), new Observer<Show>() {
+                    @Override
+                    public void onChanged(Show newShow) {
+                        show = newShow;
+                        getUserShow(show.getId());
+                        Log.i("checkShowStatus", String.valueOf(userShow));
+                        isShowSaved(tmdbId);
+                    }
+                }
+        );
+    }
+
+    private void getUserShow(Long id) {
+        viewModel.getUserShow(id).observe(getViewLifecycleOwner(), new Observer<UserShow>() {
+            @Override
+            public void onChanged(UserShow newShow) {
+                userShow = newShow;
+            }
+        });
+    }
+
+
+
+
+    public void getShowDetails(Long id){
+        viewModel.getShowDetails(id).observe(getViewLifecycleOwner(), new Observer<ShowDetails>() {
+            @Override
+            public void onChanged(ShowDetails newShowDetails) {
+                showDetails = (ShowDetails) newShowDetails;
+                bindContent(showDetails);
+            }
+        });
+    }
+
+
+    public void backButtonLogic(){
+        binding.backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager()
+                        .popBackStackImmediate();
+            }
+        });
+    }
+
+    public void isShowSaved(Long tmdbId) {
+        viewModel.isUserShowSaved(tmdbId).observe(this.getViewLifecycleOwner(), new Observer<Boolean>() {
+            @Override
+            public void onChanged(Boolean aBoolean) {
+                isSaved = aBoolean;
+                if(isSaved) {
+                    binding.ratingBar.setRating(userShow.getRating());
+                }
+            }
+        });
+    }
+
+
+    public String checkShowStatus() {
+        isShowSaved(getArguments().getLong(SHOW_ID_KEY));
+        Log.i("checkShowStatus", String.valueOf(isSaved));
+        if(isSaved) {
+            getShow(getArguments().getLong(SHOW_ID_KEY));
+            viewModel.getUserShow(show.getId());
+            getUserShow(show.getId());
+//            Log.i("filmDetails", userFilm.toString());
+            return userShow.getStatus();
+        } else {
+            return "no";
+        }
+    }
+
+
+    public void handleShowStatusLogic() {
+        Log.i("checkShowStatus", checkShowStatus());
+        isShowSaved(getArguments().getLong(SHOW_ID_KEY));
+        binding.watchedButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(checkShowStatus().equalsIgnoreCase("WATCHED")) {
+                    Toast.makeText(getContext(), "Show already watched", Toast.LENGTH_SHORT).show();
+                } else {
+                    watchedAlert();
+                }
+            }
+        });
+    }
+
+
+    public void watchedAlert() {
+        Dialog dialog = new Dialog(getContext());
+        dialog.setContentView(R.layout.watched_film_dialog);
+        dialog.setCancelable(false);
+        dialog.getWindow().getAttributes().windowAnimations = (R.style.animation);
+        dialog.findViewById(R.id.save_text).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                UserShow show = new UserShow();
+                show.setStatus("WATCHED");
+                show.setWatchedDate(LocalDate.now().toString());
+                RatingBar ratingBar = (RatingBar) dialog.findViewById(R.id.rating_in_alert);
+                show.setRating(ratingBar.getNumStars());
+                TextInputEditText notes = (TextInputEditText) dialog.findViewById(R.id.dialog_notes_input);
+                show.setNotes(notes.getText().toString());
+                if(checkShowStatus().equals("BOOKMARKED")) {
+                    viewModel.updateUserShow(viewModel.getShowDetailsByTmdbId(getArguments().getLong(SHOW_ID_KEY)).getValue().getId(), show);
+                    binding.bookmark.setChecked(false);
+                } else {
+                    viewModel.saveUserShow(getArguments().getLong(SHOW_ID_KEY), show);
+                }
+                dialog.dismiss();
+                Toast.makeText(getContext(), "Successfully Added To Watched", Toast.LENGTH_SHORT).show();
+            }
+        });
+        dialog.findViewById(R.id.cancel_text).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+
+            }
+        });
+        dialog.show();
+    }
+
 }
