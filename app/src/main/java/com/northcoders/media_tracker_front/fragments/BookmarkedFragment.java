77e56891ca81786ea184a0a68010253a72e6d196
Stef Daniela Ciocan <daniela.ciocan137@gmail.com>
2025-02-06 21:12:52 +0000

    feat: update recyclerView item logic to display filtered CommonView items (later identififed as UserFilms/Shows) instead of Fims in watched and bookmarked tabs

--- app/src/main/java/com/northcoders/media_tracker_front/fragments/BookmarkedFragment.java
+++ app/src/main/java/com/northcoders/media_tracker_front/fragments/BookmarkedFragment.java
@@ -1,6 +1,7 @@
 package com.northcoders.media_tracker_front.fragments;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,21 +21,30 @@ import com.northcoders.media_tracker_front.R;
 import com.northcoders.media_tracker_front.adapter.BookmarkedAdapter;
 import com.northcoders.media_tracker_front.adapter.RecyclerViewInterface;
 import com.northcoders.media_tracker_front.databinding.FragmentBookmarkedBinding;
+import com.northcoders.media_tracker_front.model.CommonViewItem;
 import com.northcoders.media_tracker_front.model.UserFilm;
+import com.northcoders.media_tracker_front.model.UserShow;
 import com.northcoders.media_tracker_front.viewmodel.BookmarkedViewModel;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Objects;
 
 public class BookmarkedFragment extends Fragment implements RecyclerViewInterface {
     RecyclerView recyclerView;
-    List<UserFilm> userFilmList;
+    List<CommonViewItem> itemList;
+    List<UserFilm> userFilmList = new ArrayList<>();
+    List<UserShow> userShowList = new ArrayList<>();
     BookmarkedAdapter adapter;
 
     FragmentBookmarkedBinding binding;
     BookmarkedViewModel viewModel;
+
 //    BookmarkedDetailsViewModel bookmarkedDetailsViewModel;
 
     ProfileFragment profileFragment = new ProfileFragment();
+
 
     public BookmarkedFragment() {
         // Required empty public constructor
@@ -50,7 +60,9 @@ public class BookmarkedFragment extends Fragment implements RecyclerViewInterfac
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getBookmarked();
+        getBookmarkedFilms();
+        getBookmarkedShows();
+        displayBookmarkedInRecyclerView();
     }
 
 
@@ -85,8 +97,8 @@ public class BookmarkedFragment extends Fragment implements RecyclerViewInterfac
         return binding.getRoot();
     }
 
-    private void getBookmarked() {
-        viewModel.getBookmarked().observe(getViewLifecycleOwner(), new Observer<List<UserFilm>>() {
+    private void getBookmarkedFilms() {
+        viewModel.getBookmarkedFilms().observe(getViewLifecycleOwner(), new Observer<List<UserFilm>>() {
             @Override
             public void onChanged(List<UserFilm> list) {
                 userFilmList = list;
@@ -95,9 +107,53 @@ public class BookmarkedFragment extends Fragment implements RecyclerViewInterfac
         });
     }
 
+    private void getBookmarkedShows() {
+        viewModel.getBookmarkedShows().observe(getViewLifecycleOwner(), new Observer<List<UserShow>>() {
+            @Override
+            public void onChanged(List<UserShow> list) {
+                userShowList = list;
+                Log.i("DataCheck", userShowList.toString());
+                displayBookmarkedInRecyclerView();
+            }
+        });
+    }
+
+
+
+    private List<CommonViewItem> convertData(List<UserFilm> userFilms, List<UserShow>userShows) {
+        List<CommonViewItem> recyclerList = new ArrayList<>();
+        if(!Objects.isNull(userShows) && !userShows.isEmpty()) {
+            for (UserFilm userFilm : userFilms) {
+                CommonViewItem commonViewItem = new CommonViewItem();
+                commonViewItem.setTitle(userFilm.getUserFilmId().getFilm().getTitle());
+                commonViewItem.setSynopsis(userFilm.getUserFilmId().getFilm().getSynopsis());
+                commonViewItem.setPosterUrl(userFilm.getUserFilmId().getFilm().getPoster_url());
+                commonViewItem.setType("film");
+                recyclerList.add(commonViewItem);
+            }
+        }
+        if(!Objects.isNull(userShows) && !userShows.isEmpty()) {
+            for (UserShow userShow : userShows) {
+                CommonViewItem commonViewItem = new CommonViewItem();
+                commonViewItem.setTitle(userShow.getUserShowId().getShow().getTitle());
+                commonViewItem.setSynopsis(userShow.getUserShowId().getShow().getSynopsis());
+                commonViewItem.setPosterUrl(userShow.getUserShowId().getShow().getPosterUrl());
+                commonViewItem.setType("show");
+                recyclerList.add(commonViewItem);
+            }
+        }
+        if(!recyclerList.isEmpty()) {
+            recyclerList.sort(Comparator.comparing(CommonViewItem::getTitle));
+        }
+        return recyclerList;
+    }
+
     private void displayBookmarkedInRecyclerView() {
+//        getBookmarkedShows();
+//        getBookmarkedFilms();
+        itemList = convertData(userFilmList,userShowList);
         recyclerView = binding.bookmarkedRecyclerview;
-        adapter = new BookmarkedAdapter(userFilmList, this.getContext(), this);
+        adapter = new BookmarkedAdapter(itemList, this.getContext(), this);
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this.getContext()));
         recyclerView.setHasFixedSize(true);
@@ -107,14 +163,33 @@ public class BookmarkedFragment extends Fragment implements RecyclerViewInterfac
     @Override
     public void onItemClick(int position) {
 //        FragmentTransaction transaction = getParentFragmentManager().beginTransaction();
+        if(itemList.get(position).getType().equals("film")) {
+            for(UserFilm film : userFilmList) {
+                if(film.getUserFilmId().getFilm().getTitle().equals(itemList.get(position).getTitle())) {
+                    Long id = film.getUserFilmId().getFilm().getId();
+                    BookmarkedDetailsFragment bookmarkedDetailsFragment = new BookmarkedDetailsFragment().newInstance(id);
 
-        Long id = userFilmList.get(position).getUserFilmId().getFilm().getId();
-        BookmarkedDetailsFragment bookmarkedDetailsFragment = new BookmarkedDetailsFragment().newInstance(id);
+                    getActivity().getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.frameLayoutFragment,bookmarkedDetailsFragment)
+                            .addToBackStack("BookmarkedTransaction")
+                            .commit();
+                }
+            }
+        } else {
+            for(UserShow show : userShowList) {
+                if(show.getUserShowId().getShow().getTitle().equals(itemList.get(position).getTitle())) {
+                    Long id = show.getUserShowId().getShow().getId();
+                    BookmarkedShowDetailsFragment bookmarkedShowDetailsFragment = BookmarkedShowDetailsFragment.newInstance(id);
 
-        getActivity().getSupportFragmentManager().beginTransaction()
-                .replace(R.id.frameLayoutFragment,bookmarkedDetailsFragment)
-                .addToBackStack("BookmarkedTransaction")
-                .commit();
+                    getActivity().getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.frameLayoutFragment,bookmarkedShowDetailsFragment)
+                            .addToBackStack("BookmarkedTransaction")
+                            .commit();
+                }
+            }
+        }
+//        Long id = userFilmList.get(position).getUserFilmId().getFilm().getId();
+
 //        transaction.replace(R.id.frameLayoutFragment, BookmarkedDetailsFragment.class, null);
 //        transaction.commit();
     }
--- app/src/main/java/com/northcoders/media_tracker_front/fragments/WatchedFragment.java
+++ app/src/main/java/com/northcoders/media_tracker_front/fragments/WatchedFragment.java
@@ -1,8 +1,11 @@
 package com.northcoders.media_tracker_front.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +13,7 @@ import android.widget.ImageButton;
 
 import com.bumptech.glide.Glide;
 import com.google.firebase.auth.FirebaseAuth;
+import com.northcoders.media_tracker_front.LoginActivity;
 import com.northcoders.media_tracker_front.R;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.FragmentTransaction;
@@ -17,18 +21,26 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.northcoders.media_tracker_front.adapter.BookmarkedAdapter;
 import com.northcoders.media_tracker_front.adapter.RecyclerViewInterface;
 import com.northcoders.media_tracker_front.databinding.FragmentWatchedBinding;
+import com.northcoders.media_tracker_front.model.CommonViewItem;
 import com.northcoders.media_tracker_front.model.UserFilm;
+import com.northcoders.media_tracker_front.model.UserShow;
 import com.northcoders.media_tracker_front.model.WatchHistory;
 import com.northcoders.media_tracker_front.viewmodel.WatchHistoryViewModel;
 import com.northcoders.media_tracker_front.adapter.WatchHistoryAdapter;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Objects;
 
 public class WatchedFragment extends Fragment implements RecyclerViewInterface {
     RecyclerView recyclerView;
-    ArrayList<UserFilm> watchHistoryArrayList;
+    List<CommonViewItem> watchHistoryArrayList;
+    List<UserFilm> userFilms = new ArrayList<>();
+    List<UserShow> userShows = new ArrayList<>();
     WatchHistoryAdapter watchHistoryAdapter;
     FragmentWatchedBinding binding;
     WatchHistoryViewModel viewModel;
@@ -51,7 +63,8 @@ public class WatchedFragment extends Fragment implements RecyclerViewInterface {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        getHistory();
+        getWatchedFilms();
+        getWatchedShows();
     }
 
     @Override
@@ -84,17 +97,66 @@ public class WatchedFragment extends Fragment implements RecyclerViewInterface {
         return binding.getRoot();
     }
 
-    private  void getHistory() {
+
+
+
+
+    private void getWatchedFilms() {
         viewModel.getWatchHistory().observe(getViewLifecycleOwner(), new Observer<List<UserFilm>>() {
             @Override
-            public void onChanged(List<UserFilm> historyList) {
-                watchHistoryArrayList = (ArrayList<UserFilm>) historyList;
-                displayWatchHistoryInRecyclerView();
+            public void onChanged(List<UserFilm> list) {
+                userFilms = list;
+                displayWatchedInRecyclerView();
             }
         });
     }
 
-    private void displayWatchHistoryInRecyclerView() {
+    private void getWatchedShows() {
+        viewModel.getWatchedShows().observe(getViewLifecycleOwner(), new Observer<List<UserShow>>() {
+            @Override
+            public void onChanged(List<UserShow> list) {
+                userShows = list;
+                displayWatchedInRecyclerView();
+            }
+        });
+    }
+
+
+
+    private List<CommonViewItem> convertData(List<UserFilm> userFilms, List<UserShow>userShows) {
+        List<CommonViewItem> recyclerList = new ArrayList<>();
+        if(!Objects.isNull(userFilms) && !userFilms.isEmpty()) {
+            for (UserFilm userFilm : userFilms) {
+                CommonViewItem commonViewItem = new CommonViewItem();
+                commonViewItem.setTitle(userFilm.getUserFilmId().getFilm().getTitle());
+                commonViewItem.setSynopsis(userFilm.getUserFilmId().getFilm().getSynopsis());
+                commonViewItem.setPosterUrl(userFilm.getUserFilmId().getFilm().getPoster_url());
+                commonViewItem.setWatchedDate(userFilm.getWatchedDate());
+                commonViewItem.setType("film");
+                recyclerList.add(commonViewItem);
+            }
+        }
+        if(!Objects.isNull(userShows) && !userShows.isEmpty()) {
+            for (UserShow userShow : userShows) {
+                CommonViewItem commonViewItem = new CommonViewItem();
+                commonViewItem.setTitle(userShow.getUserShowId().getShow().getTitle());
+                commonViewItem.setSynopsis(userShow.getUserShowId().getShow().getSynopsis());
+                commonViewItem.setPosterUrl(userShow.getUserShowId().getShow().getPosterUrl());
+                commonViewItem.setWatchedDate(userShow.getWatchedDate());
+                commonViewItem.setType("show");
+                recyclerList.add(commonViewItem);
+            }
+        }
+        if(!recyclerList.isEmpty()) {
+//            recyclerList.sort(Comparator.comparing(CommonViewItem::getWatchedDate));
+        }
+        return recyclerList;
+    }
+
+    private void displayWatchedInRecyclerView() {
+//        getBookmarkedShows();
+//        getBookmarkedFilms();
+        watchHistoryArrayList = convertData(userFilms,userShows);
         recyclerView = binding.recyclerview;
         watchHistoryAdapter = new WatchHistoryAdapter(watchHistoryArrayList, this.getContext(), this);
         recyclerView.setAdapter(watchHistoryAdapter);
@@ -105,12 +167,39 @@ public class WatchedFragment extends Fragment implements RecyclerViewInterface {
 
     @Override
     public void onItemClick(int position) {
-        long selectedFilmId = watchHistoryArrayList.get(position).getUserFilmId().getFilm().getId();
-                WatchedMovieFragment watchedMovieFragment = WatchedMovieFragment.newInstance(selectedFilmId);
-        getActivity().getSupportFragmentManager()
-                .beginTransaction()
-                .replace(R.id.frameLayoutFragment, watchedMovieFragment)
-                .addToBackStack(null)
-                .commit();
+//        FragmentTransaction transaction = getParentFragmentManager().beginTransaction();
+        if(watchHistoryArrayList.get(position).getType().equals("film")) {
+            for(UserFilm film : userFilms) {
+                if(film.getUserFilmId().getFilm().getTitle().equals(watchHistoryArrayList.get(position).getTitle())) {
+                    Long id = film.getUserFilmId().getFilm().getId();
+                    BookmarkedDetailsFragment bookmarkedDetailsFragment = new BookmarkedDetailsFragment().newInstance(id);
+
+                    getActivity().getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.frameLayoutFragment,bookmarkedDetailsFragment)
+                            .addToBackStack("BookmarkedTransaction")
+                            .commit();
+                }
+            }
+        } else {
+            for(UserShow show : userShows) {
+                if(show.getUserShowId().getShow().getTitle().equals(watchHistoryArrayList.get(position).getTitle())) {
+                    Long id = show.getUserShowId().getShow().getId();
+                    BookmarkedDetailsFragment bookmarkedDetailsFragment = new BookmarkedDetailsFragment().newInstance(id);
+
+                    Intent intent = new Intent(getActivity().getBaseContext(), LoginActivity.class);
+                    intent.putExtra("SHOW_ID", show.getUserShowId().getShow().getId());
+
+                    startActivity(intent);
+//                    getActivity().getSupportFragmentManager().beginTransaction()
+//                            .replace(R.id.frameLayoutFragment,bookmarkedDetailsFragment)
+//                            .addToBackStack("BookmarkedTransaction")
+//                            .commit();
+                }
+            }
+        }
+        Long id = userFilms.get(position).getUserFilmId().getFilm().getId();
+
+//        transaction.replace(R.id.frameLayoutFragment, BookmarkedDetailsFragment.class, null);
+//        transaction.commit();
     }
 }
