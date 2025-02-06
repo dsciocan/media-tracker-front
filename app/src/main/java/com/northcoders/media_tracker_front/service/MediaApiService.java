ba43393d78f4c6aebe363148cb1c43c9127422c2
Stef Daniela Ciocan <daniela.ciocan137@gmail.com>
2025-02-06 20:44:51 +0000

    feat: create show/userShow service methods for show related api endpoints

--- app/src/main/java/com/northcoders/media_tracker_front/service/MediaApiService.java
+++ app/src/main/java/com/northcoders/media_tracker_front/service/MediaApiService.java
@@ -3,23 +3,31 @@ package com.northcoders.media_tracker_front.service;
 import com.northcoders.media_tracker_front.model.Film;
 import com.northcoders.media_tracker_front.model.FilmDetails;
 import com.northcoders.media_tracker_front.model.FilmSearchResult;
+import com.northcoders.media_tracker_front.model.Show;
+import com.northcoders.media_tracker_front.model.ShowDetails;
 import com.northcoders.media_tracker_front.model.ShowSearchResult;
+import com.northcoders.media_tracker_front.model.UserFilm;
+import com.northcoders.media_tracker_front.model.UserShow;
 import com.northcoders.media_tracker_front.model.Watching;
 import java.util.List;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
 
-public interface MovieApiService {
+public interface MediaApiService {
 
     @GET("shows/search/{searchQuery}")
     Call<List<ShowSearchResult>> getShowSearchResults(@Path("searchQuery") String searchQuery);
 
+    @GET("shows/details/{showId}")
+    Call<ShowDetails> getShowDetails(@Path("showId") Long showId);
 
+    @GET("shows/saved/tmdb={tmdbId}")
+    Call<Show> getShowByTmdbId(@Path("tmdbId") Long tmdbId);
 
 
     @GET("watching")
-    Call<List<Watching>> getWatching();
+    Call<List<UserShow>> getWatching();
 
 
     @GET("films/search/{query}")
--- app/src/main/java/com/northcoders/media_tracker_front/service/UserActionsService.java
+++ app/src/main/java/com/northcoders/media_tracker_front/service/UserActionsService.java
@@ -5,6 +5,7 @@ import android.text.BoringLayout;
 import com.northcoders.media_tracker_front.model.FilmDetails;
 import com.northcoders.media_tracker_front.model.UserFilm;
 import com.northcoders.media_tracker_front.model.UserFilm;
+import com.northcoders.media_tracker_front.model.UserShow;
 
 import java.util.List;
 import java.util.Map;
@@ -15,16 +16,23 @@ import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.PATCH;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface UserActionsService {
 
+    //USER ENDPOINTS
     @GET("users/auth")
     Call<Void> auth();
 
-    @GET("users/films/search")
-    Call<List<UserFilm>> getHistory(@Query("status") String status);
+    @DELETE("users")
+    Call<Void> deleteUser();
+
+
+    //USERFILM ENDPOINTS
+    @GET("users/films/search?")
+    Call<List<UserFilm>> getFilmsByStatus(@Query("status") String status);
 
     @GET("users/films/search?status=BOOKMARKED")
     Call<List<UserFilm>> getBookmarked();
@@ -32,14 +40,11 @@ public interface UserActionsService {
     @GET("users/films/{filmId}")
     Call<UserFilm> getBookmarkedFilm(@Path("filmId") Long filmId);
 
-    @DELETE("users")
-    Call<Void> deleteUser();
-
     @GET("users/films/search?status=WATCHED")
     Call<List<UserFilm>> getHistory();
+
     @GET("users/films/{filmDbId}")
     Call<UserFilm> getUserFilmDetails(@Path("filmDbId") Long movieId );
-
 
     @PATCH("users/films/{filmDbId}")
     Call<Void> updateUserFilm(@Path("filmDbId") Long id, @Body UserFilm film);
@@ -47,21 +52,58 @@ public interface UserActionsService {
     @PATCH("users/films/{filmDbId}")
     Call<Void> updateUserBookFilm(@Path("filmDbId") Long id, @Body UserFilm film);
 
-
     @DELETE("users/films/{filmDbId}")
     Call<Void> deleteUserFilm(@Path("filmDbId") Long movieId);
-
-    @GET("users/genreStats")
-    Call<Map<String,Integer>> getUserGenreStats();
-
-    @GET("users/totalWatchedRuntime")
-    Call<Integer> getTotalWatchedRuntime();
 
     @POST("users/films/{movieid}")
     Call<UserFilm> saveUserFilm(@Path ("movieid") Long id, @Body UserFilm userFilm);
 
     @GET("users/film/{tmdbId}/isSaved")
     Call<Boolean> isUserFilmSaved(@Path("tmdbId") Long tmdbId);
+
+
+
+    //USERSHOW ENDPOINTS
+    @GET("users/shows?status=WATCHING")
+    Call<List<UserShow>> getShowsWatching(@Query("status") String status);
+
+    @GET("users/shows?status=BOOKMARKED")
+    Call<List<UserShow>> getBookmarkedShows();
+
+    @GET("users/shows?")
+    Call<List<UserShow>> getShowsByStatus(@Query("status") String status);
+
+    @GET("users/shows?status=WATCHED")
+    Call<List<UserShow>> getShowHistory();
+
+    @GET("users/shows/{showDbId}")
+    Call<UserShow> getUserShowDetails(@Path("showDbId") Long showId );
+
+    @PUT("users/shows/{showDbId}")
+    Call<Void> updateUserShow(@Path("showDbId") Long id, @Body UserShow film);
+
+//    @DELETE("users/films/{filmDbId}")
+//    Call<Void> deleteUserShow(@Path("filmDbId") Long showId);
+
+    @POST("users/shows/save/{showTmdbId}")
+    Call<UserShow> saveUserShow(@Path ("showTmdbId") Long id, @Body UserShow UserShow);
+
+    @GET("users/show/{tmdbId}/isSaved")
+    Call<Boolean> isUserShowSaved(@Path("tmdbId") Long tmdbId);
+
+
+
+
+    //STATS ENDPOINTS
+    @GET("users/genreStats")
+    Call<Map<String,Integer>> getUserGenreStats();
+
+    @GET("users/totalWatchedRuntime")
+    Call<Integer> getTotalWatchedRuntime();
+
+
+
+
 
 
 
