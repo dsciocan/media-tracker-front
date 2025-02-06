e6478a083c538811c3e5a9fd87110640ce831e8f
Stef Daniela Ciocan <daniela.ciocan137@gmail.com>
2025-02-06 20:38:17 +0000

    feat: add tmdbId/posterPath properties, data retrival methods for Show model

--- app/src/main/java/com/northcoders/media_tracker_front/model/Show.java
+++ app/src/main/java/com/northcoders/media_tracker_front/model/Show.java
@@ -7,6 +7,7 @@ public class Show {
     //TODO: datatype for genres?
 
     private Long id;
+    private Long tmdbId;
     private String title;
     private String synopsis;
     private String country;
@@ -16,15 +17,18 @@ public class Show {
     private int numberOfSeasons;
     private int numberOfEpisodes;
     private boolean isComplete;
+    private String posterUrl;
     private List<String> genres;
+
 
     //private List<Episode> episodes;
 
     public Show() {
     }
 
-    public Show(Long id, String title, String synopsis, String country, String language, int releaseYear, int finishedYear, int numberOfSeasons, int numberOfEpisodes, boolean isComplete, List<String> genres) {
+    public Show(Long id, Long tmdbId, String title, String synopsis, String country, String language, int releaseYear, int finishedYear, int numberOfSeasons, int numberOfEpisodes, boolean isComplete, String posterUrl, List<String> genres) {
         this.id = id;
+        this.tmdbId = tmdbId;
         this.title = title;
         this.synopsis = synopsis;
         this.country = country;
@@ -35,6 +39,7 @@ public class Show {
         this.numberOfEpisodes = numberOfEpisodes;
         this.isComplete = isComplete;
         this.genres = genres;
+        this.posterUrl = posterUrl;
     }
 
     public Long getId() {
@@ -43,6 +48,14 @@ public class Show {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public Long getTmdbId() {
+        return tmdbId;
+    }
+
+    public void setTmdbId(Long tmdbId) {
+        this.tmdbId = tmdbId;
     }
 
     public String getTitle() {
@@ -123,6 +136,42 @@ public class Show {
 
     public void setGenres(List<String> genres) {
         this.genres = genres;
+    }
+
+    public String getSeasonsAndEpisodes() {
+        return numberOfSeasons + " seasons | " + numberOfEpisodes + " eps";
+    }
+
+    public String getPosterUrl() {
+        return posterUrl;
+    }
+
+    public void setPosterUrl(String posterUrl) {
+        this.posterUrl = posterUrl;
+    }
+
+    public String yearAndStatus() {
+        StringBuilder builder = new StringBuilder(releaseYear);
+        builder.append("-");
+        builder.append(finishedYear);
+        builder.append(" | ");
+        if(!isComplete) {
+            builder.append("Ongoing");
+        } else {
+            builder.append("Finished");
+        }
+        return builder.toString();
+    }
+
+    public String getGenresAsString() {
+        StringBuilder genreStringBuilder = new StringBuilder();
+        for (String genre : genres) {
+            if(!genre.equals(genres.get(0))) {
+                genreStringBuilder.append(", ");
+            }
+            genreStringBuilder.append(genre);
+        }
+        return genreStringBuilder.toString();
     }
 
 }
