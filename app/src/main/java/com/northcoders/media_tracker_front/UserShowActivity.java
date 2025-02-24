440d6d79a5b1597ed896943108a7fe22be89a60a
Stef Daniela Ciocan <daniela.ciocan137@gmail.com>
2025-02-24 12:56:26 +0000

    feat: implement tabLayout and viewPager2 navigation logic

--- app/src/main/java/com/northcoders/media_tracker_front/UserShowActivity.java
+++ app/src/main/java/com/northcoders/media_tracker_front/UserShowActivity.java
@@ -1,5 +1,6 @@
 package com.northcoders.media_tracker_front;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
@@ -12,41 +13,43 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.widget.ViewPager2;
 
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+import com.northcoders.media_tracker_front.adapter.TabAdapter;
 import com.northcoders.media_tracker_front.databinding.ActivityUserShowBinding;
+import com.northcoders.media_tracker_front.fragments.EpisodeListFragment;
+import com.northcoders.media_tracker_front.fragments.SavedShowDetailsFragment;
 
 public class UserShowActivity extends AppCompatActivity {
 
-//    private AppBarConfiguration appBarConfiguration;
-//    private ActivityUserShowBinding binding;
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//
-//        binding = ActivityUserShowBinding.inflate(getLayoutInflater());
-//        setContentView(binding.getRoot());
-//
-//        setSupportActionBar(binding.toolbar);
-//
-//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_user_show);
-//        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-//
-//        binding.fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAnchorView(R.id.fab)
-//                        .setAction("Action", null).show();
-//            }
-//        });
-//    }
-//
-//    @Override
-//    public boolean onSupportNavigateUp() {
-//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_user_show);
-//        return NavigationUI.navigateUp(navController, appBarConfiguration)
-//                || super.onSupportNavigateUp();
-//    }
+
+    EpisodeListFragment episodeListFragment;
+
+    private TabAdapter tabAdapter;
+    private ViewPager2 viewPager2;
+    private static String[] tabNames = {"Details", "Episodes"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = getIntent();
+        Long id = intent.getLongExtra("SHOW_ID", 0);
+        setContentView(R.layout.activity_user_show);
+
+        viewPager2 = findViewById(R.id.viewPager);
+        tabAdapter = new TabAdapter(getSupportFragmentManager(), getLifecycle());
+        tabAdapter.addFragment(SavedShowDetailsFragment.newInstance(id));
+        tabAdapter.addFragment(EpisodeListFragment.newInstance(id));
+        viewPager2.setOrientation(ViewPager2.ORIENTATION_HORIZONTAL);
+        viewPager2.setAdapter(tabAdapter);
+
+        TabLayout tabLayout = findViewById(R.id.tab_menu);
+        new TabLayoutMediator(tabLayout, viewPager2,
+                (tab, position) -> tab.setText(tabNames[position])
+        ).attach();
+    }
+
 }
