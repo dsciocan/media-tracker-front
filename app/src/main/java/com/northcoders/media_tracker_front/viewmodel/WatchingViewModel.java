02b7f338d4bfb657a5462af686ab3a073c80adf2
Stef Daniela Ciocan <daniela.ciocan137@gmail.com>
2025-02-24 12:54:37 +0000

    feat: add watching view model methods

--- app/src/main/java/com/northcoders/media_tracker_front/viewmodel/WatchingViewModel.java
+++ app/src/main/java/com/northcoders/media_tracker_front/viewmodel/WatchingViewModel.java
@@ -1,26 +1,25 @@
 package com.northcoders.media_tracker_front.viewmodel;
-
 import android.app.Application;
-
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
-
-import com.northcoders.media_tracker_front.model.UserFilm;
 import com.northcoders.media_tracker_front.model.UserShow;
-import com.northcoders.media_tracker_front.model.Watching;
-import com.northcoders.media_tracker_front.model.repository.WatchingRepository;
-
+import com.northcoders.media_tracker_front.model.repository.UserShowRepository;
 import java.util.List;
 
 public class WatchingViewModel extends AndroidViewModel {
-    WatchingRepository watchingRepository;
+    UserShowRepository userShowRepository;
+
     public WatchingViewModel(@NonNull Application application) {
         super(application);
-        this.watchingRepository = new WatchingRepository(application);
+        this.userShowRepository = new UserShowRepository(application);
     }
 
     public LiveData<List<UserShow>> getWatching() {
-        return watchingRepository.getMutableLiveData();
+        return userShowRepository.getWatchingMutableLiveData();
+    }
+
+    public void updateShow(Long showId, UserShow newShow) {
+        userShowRepository.updateUserShow(showId, newShow);
     }
 }
