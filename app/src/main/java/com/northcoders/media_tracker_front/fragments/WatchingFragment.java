52331554e28b8246263d1c2bf412584496306b20
Stef Daniela Ciocan <daniela.ciocan137@gmail.com>
2025-02-24 13:11:20 +0000

    feat: handle recyclerview item logic, accounting for item button logic, item click naviagation

--- app/src/main/java/com/northcoders/media_tracker_front/fragments/WatchingFragment.java
+++ app/src/main/java/com/northcoders/media_tracker_front/fragments/WatchingFragment.java
@@ -1,9 +1,11 @@
 package com.northcoders.media_tracker_front.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,23 +17,33 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+import com.firebase.ui.auth.data.model.User;
+import com.google.firebase.auth.FirebaseAuth;
 import com.northcoders.media_tracker_front.R;
+import com.northcoders.media_tracker_front.UserShowActivity;
 import com.northcoders.media_tracker_front.adapter.RecyclerViewInterface;
 import com.northcoders.media_tracker_front.adapter.WatchingAdapter;
 import com.northcoders.media_tracker_front.databinding.FragmentWatchingBinding;
-import com.northcoders.media_tracker_front.model.UserFilm;
+import com.northcoders.media_tracker_front.model.UserEpisode;
 import com.northcoders.media_tracker_front.model.UserShow;
-import com.northcoders.media_tracker_front.model.Watching;
+import com.northcoders.media_tracker_front.viewmodel.UserEpisodeViewModel;
 import com.northcoders.media_tracker_front.viewmodel.WatchingViewModel;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class WatchingFragment extends Fragment implements RecyclerViewInterface {
     RecyclerView recyclerView;
-    List<UserShow> watchingList;
+    List<UserShow> watchingList  = new ArrayList<>();
     WatchingAdapter adapter;
     FragmentWatchingBinding binding;
     WatchingViewModel viewModel;
+    UserEpisodeViewModel userEpisodeViewModel;
+    Map<Long, UserEpisode> epWatchedByShow = new HashMap<>();
+
 
 
     public WatchingFragment() {
@@ -42,12 +54,13 @@ public class WatchingFragment extends Fragment implements RecyclerViewInterface 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         viewModel = new ViewModelProvider(this).get(WatchingViewModel.class);
-        //getWatching();
+        userEpisodeViewModel = new ViewModelProvider(this).get(UserEpisodeViewModel.class);
     }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWatching();
     }
 
     @Override
@@ -58,26 +71,70 @@ public class WatchingFragment extends Fragment implements RecyclerViewInterface 
     }
 
     private void getWatching() {
-        viewModel.getWatching().observe(this, new Observer<List<UserShow>>() {
+        viewModel.getWatching().observe(this.getViewLifecycleOwner(), new Observer<List<UserShow>>() {
             @Override
             public void onChanged(List<UserShow> watching) {
                 watchingList = watching;
+                Log.i("EP", epWatchedByShow.toString());
                 displayWatchingInRecyclerView();
             }
         });
     }
 
     private void displayWatchingInRecyclerView() {
-        recyclerView = binding.watchingRecyclerview;
-        adapter = new WatchingAdapter(watchingList, this.getContext(), this);
-        recyclerView.setAdapter(adapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this.getContext()));
-        recyclerView.setHasFixedSize(true);
-        adapter.notifyDataSetChanged();
+        if(watchingList != null && epWatchedByShow != null) {
+            recyclerView = binding.watchingRecyclerview;
+            adapter = new WatchingAdapter(watchingList, this.getContext(), this, epWatchedByShow);
+            recyclerView.setAdapter(adapter);
+            recyclerView.setLayoutManager(new LinearLayoutManager(this.getContext()));
+            recyclerView.setHasFixedSize(true);
+            watchingItemNextClickListener();
+            adapter.notifyDataSetChanged();
+        }
     }
 
-    @Override
+
+    private void watchingItemNextClickListener() {
+        adapter.setOnClickListener(new WatchingAdapter.OnClickListener() {
+            @Override
+            public void onNextClick(View view, int position) {
+                UserShow show = watchingList.get(position);
+                getLatestEpisode(show.getUserShowId().getShow().getId());
+                show.setEpisodesWatched(show.getEpisodesWatched() + 1);
+                viewModel.updateShow(show.getUserShowId().getShow().getId(), show);
+            }
+        });
+    }
+
+    public void getLatestEpisode(Long showId) {
+            userEpisodeViewModel.getLatestEpisode(showId).observe(getViewLifecycleOwner(), new Observer<UserEpisode>() {
+                @Override
+                public void onChanged(UserEpisode episode) {
+                    epWatchedByShow.put(showId, episode);
+                    Log.i("EP", showId + ": " + episode.getUserEpisodeId().getEpisode().getEpisodeNumber());
+                    updateLatestEpisode(episode);
+                }
+            });
+        }
+
+
+        public void latestEpisodes() {
+            displayWatchingInRecyclerView();
+        }
+
+        @Override
     public void onItemClick(int position) {
 
+        Long id = watchingList.get(position).getUserShowId().getShow().getId();
+
+        Intent intent = new Intent(getActivity().getBaseContext(), UserShowActivity.class);
+        intent.putExtra("SHOW_ID", id);
+        startActivity(intent);
+    }
+
+    public void updateLatestEpisode(UserEpisode episode) {
+        episode.setWatched(true);
+        userEpisodeViewModel.updateUserEpisode(episode.getUserEpisodeId().getEpisode().getId(), episode);
+        displayWatchingInRecyclerView();
     }
 }
